0de65c7bc48af6a4bbba1b1f839123ee0e849c07
URUMI-PC\Urumi\Desktop <polcracker13@gmail.com>
2016-11-10 20:32:40 +0300

    1
    nice try

---
+++ Project/GrabInteraction.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+
+class CGrab(object):
+    def __init__(self):
+        pass
---
+++ Project/Logger.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+
+class CLogger(object):
+    def __init__(self):
+        pass
